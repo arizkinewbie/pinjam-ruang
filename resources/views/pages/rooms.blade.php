5dea6314fc7669abfbf51710802554da0528660e
arizkinewbie <arizkijitsu@gmail.com>
2024-12-20 20:00:45 +0700

    fix: delete image and rating

--- resources/views/pages/rooms.blade.php
+++ resources/views/pages/rooms.blade.php
@@ -66,20 +66,21 @@
                     @endphp
                     <div class="col-lg-6">
                         <div class="room-wrap d-md-flex">
-                            <a href="#" class="img"
-                                style="background-image: url({{ asset('vendor/technext/vacation-rental/images/room-' . rand(1, 6) . '.jpg') }});"></a>
                             <div class="half left-arrow d-flex align-items-center">
                                 <div class="text p-4 p-xl-5 text-center">
-                                    <p class="star mb-0"><span class="fa fa-star"></span><span
-                                            class="fa fa-star"></span><span class="fa fa-star"></span><span
-                                            class="fa fa-star"></span><span class="fa fa-star"></span></p>
                                     <p class="mb-0">{{ $room->room_type->name }}</p>
                                     <h3 class="mb-3"><a href="rooms.html">{{ $room->name }}</a></h3>
                                     <ul class="list-accomodation">
                                         <li><span>Maks:</span> {{ $room->max_people }} Orang</li>
                                         <li><span>Status:</span> {{ App\Enums\RoomStatus::getDescription($room_status) }}
                                         </li>
-                                        <li>{!! implode('<br>', $borrower_status) !!}</li>
+                                        <li>
+                                            @if (count($borrower_status) == 0)
+                                                <br>
+                                            @else
+                                                {!! implode('<br>', $borrower_status) !!}
+                                            @endif
+                                        </li>
                                     </ul>
                                     <p class="pt-1"><a href="javascript:void(0)" id="buttonBorrowRoomModal"
                                             class="btn-custom px-3 py-2" data-toggle="modal" data-target="#borrowRoomModal"
